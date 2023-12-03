a1ad47ca59519e48ad6ff131b6e1acb9d321caab
mahejbeen <mahejbeen193@gmail.com>
2023-12-03 13:04:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+// const port = 3000
+const port = process.env.PORT;
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+
+app.get('/twitter', (req, res) => {
+    res.send('<h1>Hello World! youtube</h1>')
+  })
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
